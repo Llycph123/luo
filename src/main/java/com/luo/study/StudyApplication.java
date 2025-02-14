f907a5f57b26541717f0e891650905409125dfa4
luolingyan <1150944229@qq.com>
2025-02-15 00:21:17 +0800

    create

---
+++ src/main/java/com/luo/study/StudyApplication.java
@@ -0,0 +1,16 @@
+package com.luo.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luolingyan
+ * @description StudyApplication
+ * @create 2025-02-14 23:41
+ */
+@SpringBootApplication
+public class StudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+}
