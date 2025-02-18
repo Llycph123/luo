12951e7d2e280b8642a0e1fb96972654840583aa
luolingyan <1150944229@qq.com>
2025-02-18 17:02:16 +0800

    responsibility

---
+++ src/main/java/com/luo/study/mode/responsibility/handler/CEO.java
@@ -0,0 +1,17 @@
+package com.luo.study.mode.responsibility.handler;
+
+/**
+ * @author luolingyan
+ * @description CEO，可以批准55%以内的折扣，折扣超出55%，就拒绝申请
+ * @create 2025-02-18 14:38
+ */
+public class CEO extends PriceHandler{
+    @Override
+    public void processDiscount(float discount) {
+        if(discount <= 0.55){
+            System.out.format("%s批准了折扣：%.4f%n", this.getClass().getName(),discount);
+        }else{
+            System.out.format("%s拒绝了折扣：%.4f%n", this.getClass().getName(),discount);
+        }
+    }
+}
---
+++ src/main/java/com/luo/study/mode/responsibility/handler/PriceHandlerFactory.java
@@ -0,0 +1,29 @@
+package com.luo.study.mode.responsibility.handler;
+
+/**
+ * @author luolingyan
+ * @description
+ * @create 2025-02-18 15:06
+ */
+public class PriceHandlerFactory {
+    /**
+     * 创建PriceHandler的工厂方法
+     * @return
+     */
+    public static PriceHandler createPriceHandler() {
+        PriceHandler sales = new Sales();
+        PriceHandler lead = new Lead();
+        PriceHandler manager = new Manager();
+        PriceHandler director = new Director();
+        PriceHandler vicePresident = new VicePresident();
+        PriceHandler ceo = new CEO();
+
+        sales.setSuccessor(lead);
+        lead.setSuccessor(manager);
+        manager.setSuccessor(director);
+        director.setSuccessor(vicePresident);
+        vicePresident.setSuccessor(ceo);
+
+        return sales;
+    }
+}
